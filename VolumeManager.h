72360d3d27b76635824ac40a7d16d82a82ff566e
AntaresOne <tesla.madal93@gmail.com>
2014-07-27 19:14:18 +0200

    Update

--- VolumeManager.h
+++ VolumeManager.h
@@ -154,6 +154,8 @@ private:
     void readInitialState();
     bool isMountpointMounted(const char *mp);
     bool isAsecInDirectory(const char *dir, const char *asec) const;
+    bool isLegalAsecId(const char *id) const;
+    int openLun(int number);
 };
 
 extern "C" {
